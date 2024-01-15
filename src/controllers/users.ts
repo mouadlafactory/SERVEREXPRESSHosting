f50825a3ce7e1a5900de10621786c590602d10b3
Mouad Bounfil <mouad.bounfil.it@gmail.com>
2024-01-15 18:11:51 +0100

    add api /

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -3,6 +3,17 @@ import PersonModel from "../db/users";
 
 import { deleteUserById, getUsers, getUserById , getUserList } from '../db/users';
 
+
+export const hiApi = async (req: express.Request, res: express.Response) => {
+  try {
+    const users = await getUsers();
+
+    return res.status(200).json(users);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
 export const getAllUsers = async (req: express.Request, res: express.Response) => {
   try {
     const users = await getUsers();
--- src/router/users.ts
+++ src/router/users.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 
-import { getAllUsers, deleteUser, updateUser , getUserListHandler , getUser  } from '../controllers/users';
+import { getAllUsers, deleteUser, updateUser , getUserListHandler , getUser  , hiApi} from '../controllers/users';
 
 export default (router: express.Router) => {
+  router.get("/", hiApi)
   router.get('/users', getAllUsers);
   router.get('/user', getUserListHandler);
   router.get('/user/profile/:dataUser', getUser);
